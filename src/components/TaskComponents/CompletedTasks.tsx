9a81b4543545103b9a5a4dee3a2a2cd8b8bdfb3b
pradiptechie <kumarpradip3956@gmail.com>
2024-07-14 08:06:26 +0545

    pedning , Inprogress and Completed section is done

--- src/components/TaskComponents/CompletedTasks.tsx
+++ src/components/TaskComponents/CompletedTasks.tsx
@@ -1,4 +1,6 @@
 import { useTaskContext } from "../../context/TaskContext";
+import TaskItem from "./TaskItems";
+import { Box, Paper, Grid, Container, Typography } from "@mui/material";
 
 export default function CompletedTasks() {
   const { tasks } = useTaskContext();
@@ -6,9 +8,29 @@ export default function CompletedTasks() {
 
   console.log("completedTasks", completedTasks);
   return (
-    <div>
-      <h1>CompletedTasks</h1>
-      <h1>{}</h1>
-    </div>
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Completed Tasks
+      </Typography>
+      <Box
+        component={Paper}
+        elevation={3}
+        sx={{
+          borderRadius: 2,
+          width: "full",
+          minHeight: "450px",
+          padding: 1,
+          backgroundColor: "#fafafa",
+        }}
+      >
+        <Grid container rowSpacing={1} columnSpacing={2}>
+          {completedTasks.map((task) => (
+            <Grid item xs={12} sm={6} key={task._id}>
+              <TaskItem task={task} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
   );
 }
--- src/components/TaskComponents/InProgressTasks.tsx
+++ src/components/TaskComponents/InProgressTasks.tsx
@@ -1,12 +1,36 @@
 import { useTaskContext } from "../../context/TaskContext";
+import TaskItem from "./TaskItems";
+import { Box, Paper, Grid, Container, Typography } from "@mui/material";
 
 export default function InProgressTasks() {
   const { tasks } = useTaskContext();
   const inProgressTasks = tasks.filter((task) => task.status === "in-progress");
   console.log("inProgressTasks", inProgressTasks);
   return (
-    <div>
-      <h1>InProgressTasks</h1>
-    </div>
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        In-Progress Tasks
+      </Typography>
+      <Box
+        component={Paper}
+        elevation={3}
+        sx={{
+          borderRadius: 2,
+          width: "full",
+          minHeight: "450px",
+          padding: 1,
+          backgroundColor: "#fafafa",
+        }}
+      >
+        <Grid container rowSpacing={1} columnSpacing={2}>
+          {inProgressTasks.map((task) => (
+            <Grid item xs={12} sm={6} key={task._id}>
+              <TaskItem task={task} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
   );
 }
+
